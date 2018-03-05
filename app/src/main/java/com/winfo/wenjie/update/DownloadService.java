2368b32e6e983eaeb4c1fe384c2c67cdb643e749
wj576038874 <1rujiwang>
2018-03-05 17:31:20 +0800

    commit

--- app/src/main/java/com/winfo/wenjie/update/DownloadService.java
+++ app/src/main/java/com/winfo/wenjie/update/DownloadService.java
@@ -6,7 +6,10 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.Builder;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 
 import java.io.File;
@@ -36,13 +39,12 @@ public class DownloadService extends IntentService {
     protected void onHandleIntent(Intent intent) {
 
         mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mBuilder = new Builder(this);
-
+        mBuilder = new NotificationCompat.Builder(this);
 
         String appName = getString(getApplicationInfo().labelRes);
         int icon = getApplicationInfo().icon;
-        mBuilder.setContentTitle(appName)
-                .setSmallIcon(icon);
+
+        mBuilder.setContentTitle(appName).setSmallIcon(icon);
         String urlStr = intent.getStringExtra(Constants.APK_DOWNLOAD_URL);
         InputStream in = null;
         FileOutputStream out = null;
@@ -64,7 +66,6 @@ public class DownloadService extends IntentService {
             int byteread = 0;
             in = urlConnection.getInputStream();
             File dir = StorageUtils.getCacheDirectory(this);
-//            String path = Environment.getExternalStorageDirectory()+"/updateVersion/gdmsaec-app.apk";
             String apkName = urlStr.substring(urlStr.lastIndexOf("/") + 1, urlStr.length());
             File apkFile = new File(dir, apkName);
             out = new FileOutputStream(apkFile);
@@ -85,12 +86,12 @@ public class DownloadService extends IntentService {
             }
             // 下载完成
 
-            installAPk(apkFile);
+            installAPk(this, apkFile);
 
             mNotifyManager.cancel(NOTIFICATION_ID);
 
         } catch (Exception e) {
-            Log.e(TAG, "download apk file error");
+            Log.e(TAG, "download apk file error:" + e.getMessage());
         } finally {
             if (out != null) {
                 try {
@@ -111,9 +112,7 @@ public class DownloadService extends IntentService {
 
     private void updateProgress(int progress) {
         //"正在下载:" + progress + "%"
-        mBuilder.setContentText(this.getString(R.string.android_auto_update_download_progress, progress))
-                .setWhen(System.currentTimeMillis())
-                .setProgress(100, progress, false);
+        mBuilder.setContentText(this.getString(R.string.android_auto_update_download_progress, progress)).setProgress(100, progress, false);
         //setContentInent如果不设置在4.0+上没有问题，在4.0以下会报异常
         PendingIntent pendingintent = PendingIntent.getActivity(this, 0, new Intent(), PendingIntent.FLAG_CANCEL_CURRENT);
         mBuilder.setContentIntent(pendingintent);
@@ -121,9 +120,31 @@ public class DownloadService extends IntentService {
     }
 
 
-    private void installAPk(File apkFile) {
+    private void installAPk(Context context, File apkFile) {
+        Intent installAPKIntent = getApkInStallIntent(context, apkFile);
+        startActivity(installAPKIntent);
+    }
+
+    private Intent getApkInStallIntent(Context context, File apkFile) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        //如果没有设置SDCard写权限，或者没有sdcard,apk文件保存在内存中，需要授予权限才能安装
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            Uri uri = FileProvider.getUriForFile(context, context.getPackageName() + ".update.provider", apkFile);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        } else {
+            Uri uri = getApkUri(apkFile);
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        }
+        return intent;
+    }
+
+
+    private Uri getApkUri(File apkFile) {
+        Log.d(TAG, apkFile.toString());
+
+        //如果没有设置 SDCard 写权限，或者没有 SDCard,apk 文件保存在内存中，需要授予权限才能安装
         try {
             String[] command = {"chmod", "777", apkFile.toString()};
             ProcessBuilder builder = new ProcessBuilder(command);
@@ -131,10 +152,9 @@ public class DownloadService extends IntentService {
         } catch (IOException ignored) {
         }
         Uri uri = Uri.fromFile(apkFile);
-        intent.setDataAndType(uri, "application/vnd.android.package-archive");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        Log.d(TAG, uri.toString());
 
+        return uri;
     }
 
 }
